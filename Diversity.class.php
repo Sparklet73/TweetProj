0a169abe3cb61b6f70ce18017e5c6591344a5db1
Sparklet73 <talcy.73@gmail.com>
2015-01-28 23:20:28 +0800

    revise function of Diversity.class.php

--- Diversity.class.php
+++ Diversity.class.php
@@ -8,14 +8,15 @@
 
 class Diversity {
 
-    // get user diversity per hasthag
+    // get user diversity per hashtag
     public function userDiversity($usersForWord, $distinctUsersForWord) {
         //$userDiversity[$date][$word] = round(($distinctUsersForWord[$date][$word] / $usersForWord[$date][$word]) * 100, 2);
     }
 
-    // what to focus? user or hashtag?
-    public function retweetedUserDiversity() {
-
+	// get mention diversity per user in this period
+	//SELECT `to_user`, count(`from_user_id`), count(distinct(`from_user_id`)) as c FROM `HK831_mentions` GROUP BY `to_user_id` ORDER BY c DESC
+    public function mentionDiversity() {
+		//$mentionDiversity[$date][$user] = round(($distinctUsers[$date][$rtUser] / $users[$date][$rtUser]) * 100, 2);
     }
 
     public function languageDiversity() {
