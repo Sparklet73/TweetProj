844c51a363e58bafb612539f698c8f7f74e1e285
Sparklet73 <talcy.73@gmail.com>
2015-02-05 18:59:24 +0800

    write into database

--- Diversity.class.php
+++ Diversity.class.php
@@ -9,6 +9,7 @@
 class Diversity {
 
     private $dbh = NULL;
+    private $strBinName = NULL;
     private $strUserDivName = NULL;
     private $strMentionDivName = NULL;
     private $arrUserDiversity = array();
@@ -24,6 +25,7 @@ class Diversity {
         if (empty($strDBPrefix)) {
             throw new Exception('Database prefix name is empty!');
         }
+        $this->strBinName = $strDBPrefix;
         $this->strUserDivName = $strDBPrefix . '_UserDiv';
         $this->strMentionDivName = $strDBPrefix . '_MentionDiv';
     }
@@ -38,7 +40,6 @@ class Diversity {
             `usrCount` int(10) unsigned NOT NULL,
             `uniqueUsrCount` int(10) unsigned NOT NULL,
             PRIMARY KEY (`UDId`),
-            KEY `hashtag` (`hashtag`)
             ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;";
         try {
             $stmt = $this->dbh->prepare($sql_init);
@@ -58,7 +59,6 @@ class Diversity {
             `rtUsrCount` int(10) unsigned NOT NULL,
             `uniqueRtUsrCount` int(10) unsigned NOT NULL,
             PRIMARY KEY (`MDId`),
-            KEY `from_usr_name` (`from_usr_name`)
             ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;";
         try {
             $stmt = $this->dbh->prepare($sql_init);
@@ -71,8 +71,8 @@ class Diversity {
     // get user diversity per hashtag
     public function userDiversity () {
         $sql = "SELECT LOWER(h.text) h1, DATE_FORMAT(created_at,'%Y-%m-%d') datepart, COUNT(from_user_id) c, COUNT(DISTINCT(from_user_id)) d
-                FROM HK831_hashtags h
-                GROUP BY h1, datepart";
+                FROM `" . $this->strBinName . "_hashtags` h
+                GROUP BY h1, datepart;";
         try {
             $stmt = $this->dbh->prepare($sql);
             $stmt->execute();
@@ -93,19 +93,31 @@ class Diversity {
             $arrUsersForHashtag[$date][$hashtag] = $row['c'];
             $arrDistinctUsersForHashtag[$date][$hashtag] = $row['d'];
         }
+        $sql_write = "REPLACE INTO `" . $this->strUserDivName . "`
+                        VALUES(NULL,:hashtag,:date,:usrDiv,:usrCount,:uniqueUserCount);";
+
         foreach ($arrDistinctUsersForHashtag as $date => $hashtags) {
             foreach ($hashtags as $hashtag => $distinctUserCount) {
                 // (number of unique users using the hashtag) / (frequency of use)
                 $this->arrUserDiversity[$date][$hashtag] = round(($arrDistinctUsersForHashtag[$date][$hashtag] / $arrUsersForHashtag[$date][$hashtag]), 2);
+
+
+                $sql_write->bindParam(':hashtag', $hashtag, PDO::PARAM_STR);
+                $sql_write->bindParam(':date', $date, PDO::PARAM_STR);
+                $sql_write->bindParam(':usrDiv', $this->arrUserDiversity[$date][$hashtag], PDO::Param_)
             }
         }
+
+
+
+
     }
 
 	// get rt/mention diversity per user
     public function mentionDiversity() {
         $sql = "SELECT `to_user` usr, DATE_FORMAT(created_at,'%Y-%m-%d') datepart, count(`from_user_name`) c, count(distinct(`from_user_name`)) d
-                FROM `HK831_mentions`
-                GROUP BY `to_user_id`";
+                FROM `" . $this->strBinName . "_mentions`
+                GROUP BY `to_user_id`;";
         try {
             $stmt = $this->dbh->prepare($sql);
             $stmt->execute();
@@ -124,9 +136,19 @@ class Diversity {
                 $this->arrMentionDiversity[$date][$usr] = round(($arrDistinctRtUsersCount[$date[$usr]] / $arrRtUsersCount[$date][$usr]), 2);
             }
         }
+
     }
 
-    public function VocabularyDiversity() {
+    public function vocabularyDiversity() {
+
+    }
+
+    // link hashtags and mentionee with tweet
+    public function relationHashtagMentionee() {
+        $sql = "SELECT  h.`tweet_id`, h.`text`,h.`from_user_name`,m.`to_user`
+                FROM `" . $this->strBinName . "_hashtags` h, `" . $this->strBinName . "_mentions` m
+                WHERE h.`tweet_id`=m.`tweet_id`
+                GROUP BY h.`tweet_id`";
 
     }
 }
